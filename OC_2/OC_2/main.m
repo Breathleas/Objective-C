b569da4b409569d5ca8bc735f33fa606595dd6cc
pudongqi <pudongqi@gmail.com>
2017-11-14 17:35:43 +0800

    上传

---
+++ OC_2/OC_2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC_2
+//
+//  Created by yuliu on 2017/11/14.
+//  Copyright © 2017年 1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
